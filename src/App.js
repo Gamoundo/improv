904da416fb4b09c126db41af929c4e2ca97b00e9
Gamoundo <akilgg@gmail.com>
2020-11-20 17:50:20 -0500

    added user and userdisplay to game component

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ render() {
     <div className="App">
       <img src="https://pm1.narvii.com/5718/5b8a185d6f2cdb082383682653251e5d18131292_00.jpg" alt="anime speech search"/>
       <h1>Orator</h1>
-      <h3>Rules</h3>
-      <h4> {this.state.name} </h4>
+      <h3>{this.state.name}</h3>
+      <h4> Rules </h4>
       <p> basically you have three minutes to construct a story using the provided situation and help. your goal is to make your audience feel the emotion shown to you.</p>
         <div className="situation">
         
@@ -29,6 +29,7 @@ render() {
       {this.state.name !== "" && <button> Join Game</button>}
       {this.state.name !== "" && <button> Create Game</button>}
       {this.state.name == "" && <Login />}
+      <Game />
     </div>
   );
 }
--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,27 @@
-import { render } from '@testing-library/react';
+
 import React from 'react';
 import Timer from './Timer'
 
 class Game extends React.Component{
-    
+    state = {
+      users : [{name: "Tom", id: 1, points: 20}, {name: "Tony", id: 2, points: 15}, {name: "Joe", id: 3, points: 100}]
+    }
 
  changeStory() {
   window.location = '/'
-} render() {
+} 
+
+displayUsers()  {
+return (this.state.users.map((user) => {
+  return(
+    <div className="user"> <h3> {user.name}</h3>
+    <p>{user.points}</p>
+    </div>
+  )
+}))
+}
+
+render() {
     let situations = ["shark attack", "stubbed toe", "totaled boss's car", "vengeful ghost", "caught committing crime", "It is M-fing Monday!", "Surrounded by clowns", "World ends in a hour", "Witnessed an alien abduction", "Ran out of goldfish", "trapped with a shapehifter", "Out of coffee"]
 let help = ["graham crackers", "interpretive dance", "diplomacy", "hidden bobby pin", "smoke bomb", "hug", "singing", "pineapple pizza", "black belt in..."]
 
@@ -17,6 +31,7 @@ return (
       <img src="https://pm1.narvii.com/5718/5b8a185d6f2cdb082383682653251e5d18131292_00.jpg" alt="anime speech search"/>
       <h1>Orator</h1>
       <Timer/>
+<div className="users"> {this.displayUsers()}</div>
         <div className="situation">
         <p> Situation: {situations[Math.floor(Math.random()* situations.length)]}</p>
       <p> Help: {help[Math.floor(Math.random()* help.length)]}</p>
