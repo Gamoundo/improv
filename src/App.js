7a8eea6e398f3386332f1168e75f4060f8909ae0
Gamoundo <akilgg@gmail.com>
2020-12-13 23:51:09 -0500

    fixing the display of the app page

--- src/App.js
+++ src/App.js
@@ -42,27 +42,42 @@ createGame= () =>{
 
 
 render() {
-  return (
-    <Router>
-      <div className="App">
-      <img src="https://pm1.narvii.com/5718/5b8a185d6f2cdb082383682653251e5d18131292_00.jpg" alt="anime speech search"/>
-      <h1>Orator</h1>
-      <h3>{this.state.name}</h3>
-      <h4> Rules </h4>
-      <p> basically you have three minutes to construct a story using the provided situation and help. your goal is to make your audience feel the emotion shown to you.</p>
-        <div className="situation">
-        
-        </div>
-      {this.state.name !== "" && <button onClick={this.playGame}> Join Game</button>}
-      {this.state.name !== "" && <button onClick= {this.createGame}> Create Game</button>}
-      {this.state.name === "" && <Login />}
-      {/* <Game /> */}
-      <Route exact path="/Game/:id" component= {Game}/>
-      <Logo />
-    </div>
-    </Router>
+  if (window.location = '/home') {
+    return (
     
-  );
+      <Router>
+        <div className="App">
+        <img src="https://pm1.narvii.com/5718/5b8a185d6f2cdb082383682653251e5d18131292_00.jpg" alt="anime speech search"/>
+        <h1>Orator</h1>
+        <h3>{this.state.name}</h3>
+        <h4> Rules </h4>
+        <p> basically you have three minutes to construct a story using the provided situation and help. your goal is to make your audience feel the emotion shown to you.</p>
+          <div className="situation">
+          
+          </div>
+        {this.state.name !== "" && <button onClick={this.playGame}> Join Game</button>}
+        {this.state.name !== "" && <button onClick= {this.createGame}> Create Game</button>}
+        {this.state.name === "" && <Login />}
+        {/* <Game /> */}
+        <Route exact path="/home" component= {App}/>
+        <Route exact path="/Game/:id" component= {Game}/>
+        <Logo />
+      </div>
+      </Router>
+      
+    );   
+  } else {
+    return (
+      <Router>
+        <div>
+        <Route exact path="/home" component= {App}/>
+        <Route exact path="/Game/:id" component= {Game}/>
+        <Logo />
+        </div>
+      </Router>
+    )
+  }
+  
 }
 
   
